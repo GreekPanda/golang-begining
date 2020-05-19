4163c2a005a61fc752d1185957006b7e90ec67ff
liaohuanghe <271799648@qq.com>
2020-05-20 00:13:38 +0800

    1.SOme test of function

---
+++ function/test/funcTest.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(n1, n2 float32)  float32 {
+	return n1 + n2
+}
+
+func swap(n1 *int, n2 *int) {
+
+	tmp := *n1
+	*n1 = *n2
+	*n2 = tmp
+}
+
+func main()  {
+	fmt.Println(sum(1,2))
+	fmt.Println("---------")
+	n1 := 10
+	n2 := 20
+	swap(&n1, &n2)
+	fmt.Printf("n1 = %v, n2 = %v\n", n1, n2)
+}
