300efb65ab7c2c2d2f73c35186c3e6ea0a9c6a3c
liaohuanghe <271799648@qq.com>
2020-05-21 08:54:09 +0800

    1.使用了全局匿名函数

--- function/anonymous/anony.go
+++ function/anonymous/anony.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+//全局匿名函数必须是大写，默认都是课件才能使用
+var (
+	Fun1 = func(n1, n2 int) int{
+		return n1 * n2
+	}
+)
+
 func main()  {
 	//求和使用匿名函数
 	//方法1
@@ -19,4 +26,9 @@ func main()  {
 
 	ret2 := sub(20, 10)
 	fmt.Println(ret2)
+
+	//方法3 ，全局匿名函数
+
+	ret3 := Fun1(10, 2)
+	fmt.Println(ret3)
 }
