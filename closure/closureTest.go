a3efb594a84ab3344e708157640971eda03432c4
liaohuanghe <271799648@qq.com>
2020-05-21 23:50:32 +0800

    1.测试闭包

--- closure/closureTest.go
+++ closure/closureTest.go
@@ -5,6 +5,9 @@ import (
 	"strings"
 )
 
+//返回的匿名函数和suffix变量构成了一个闭包
+//闭包的好处传入的公共变量一次就够了，而非闭包就是需要每次都需要传入
+//闭包的优势在于可以延续上次调用的返回结果，减少了重复的传入
 func makeSuffix(suffix string) func (string) string  {
 	return func (name string) string {
 		if !strings.HasSuffix(name, suffix) {
